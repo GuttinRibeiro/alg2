ec64b9257978f9d906c95e965971335e2f930180
Augusto Ribeiro <augustoribeiro97@gmail.com>
2017-09-15 17:41:37 -0300

    Ajacency Matrix implemented

--- adjacencyMatrix.c
+++ adjacencyMatrix.c
@@ -1,3 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "adjacencyMartrix.h"
+#include "adjacencyMatrix.h"
+
+void initGraph(Graph *g, int numVertex) {
+  g->numVertex = numVertex;
+  for(int i = 0; i < g->numVertex; i++) {
+    for(int j = 0; j < g->numVertex; j++) {
+      g->matrix[i][j] = 0.0;
+    }
+  }
+}
+
+int insertLine(Graph *g, block u, block v, weight value) {
+  if(u > g->numVertex || v > g->numVertex) {
+    return -1;
+  }
+
+  g->matrix[u][v] = value;
+  return 1;
+}
+
+int checkLine(Graph *g, block u, block v) {
+  if(u > g->numVertex || v > g->numVertex) {
+    return -1;
+  }
+
+  if(g->matrix[u][v] > 0.0) {
+    return 1;
+  }
+
+  return 0;
+}
+
+int removeLine(Graph *g, block u, block v) {
+  if(u > g->numVertex || v > g->numVertex) {
+    return -1;
+  }
+
+  g->matrix[u][v] = 0.0;
+  return 1;
+}
+
+int checkIfThereIsANeighboor(Graph *g, block u) {
+  if(u > g->numVertex) {
+    return -1;
+  }
+
+  for(int i = 0; i < g->numVertex; i++) {
+    if(g->matrix[u][i] > 0.0) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+int getFirstNeighboor(Graph *g, block u) {
+  if(u > g->numVertex) {
+    return -1;
+  }
+
+  for(int i = 0; i < g->numVertex; i++) {
+    if(g->matrix[u][i] > 0.0) {
+      return i;
+    }
+  }
+
+  return 0;
+}
--- adjacencyMatrix.h
+++ adjacencyMatrix.h
@@ -7,14 +7,14 @@ typedef int block;
 typedef float weight;
 
 typedef struct {
-  block matrix[MAXNUMVERTEX][MAXNUMVERTEX];
+  weight matrix[MAXNUMVERTEX][MAXNUMVERTEX];
   int numVertex;
 } Graph;
 
 void initGraph(Graph *g, int numVertex);
-void insertLine(Graph *g, block u, block v, weight value);
+int insertLine(Graph *g, block u, block v, weight value);
 int checkLine(Graph *g, block u, block v);
-void removeLine(Graph *g, block u, block v);
+int removeLine(Graph *g, block u, block v);
 int checkIfThereIsANeighboor(Graph *g, block u);
 int getFirstNeighboor(Graph *g, block u);
 
