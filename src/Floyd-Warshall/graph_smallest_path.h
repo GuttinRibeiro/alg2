e89a425ef5cf03258274a61110b7c8fea0516b36
HSuzu <henry.shinji@gmail.com>
2017-09-19 23:12:19 -0300

    Crazyness made me test for defines on typedefs... Fixed

--- src/Floyd-Warshall/graph_smallest_path.h
+++ src/Floyd-Warshall/graph_smallest_path.h
@@ -1,18 +1,6 @@
 #ifndef GRAPH_SMALLEST_PATH_H
 #define GRAPH_SMALLEST_PATH_H
 
-#ifndef Graph
-#error Unknown definition of Graph.
-#endif
-
-#ifndef weight
-#error Unknown definition of weight.
-#endif
-
-#ifndef vertex
-#error Unknown definition of vertex.
-#endif
-
 #ifndef INF
 #define INF 10000
 #warning Please define the infinity value of weight.
