32dfbf7687c14e8f0d41f2fdf492fc1b426f61c1
Augusto Ribeiro <augustoribeiro97@gmail.com>
2017-09-20 10:59:19 -0300

    Henry, lá vai a main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "src/adjacencyMatrix/adjacencyMatrix.h"
+#include "src/GraphUtils/graph_utils.h"
+
+int main () {
+  Graph g;
+  initGraph(&g, 5);
+  insertDirectedLine(&g, 0, 1, 1.0);
+  insertDirectedLine(&g, 1, 2, 2.0);
+  insertDirectedLine(&g, 2, 3, 4.0);
+  insertDirectedLine(&g, 3, 4, 5.0);
+  insertDirectedLine(&g, 4, 1, 1.0);
+  insertDirectedLine(&g, 2, 4, 2.0);
+  insertDirectedLine(&g, 4, 2, 3.0);
+
+  vertex central = 0;
+  GraphCentrality(&g, &central);
+
+  printf("Central: %d\n", central);
+  return 0;
+}
